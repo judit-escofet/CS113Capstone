7e6e8cbfff96e77ab718a4d60b26f77443a1ae45
Nino Chkhaidze <chkhaidzenintsi@gmail.com>
2026-05-03 16:13:06 -0400

    Added Rivian R1T ElectricCar to vehicles[3]

--- Garage.java
+++ Garage.java
@@ -7,6 +7,7 @@ public class Garage
         vehicles[0] = new LightTruck("Ford", "F-150", 2020, Vehicle.nextVehicleID, true, 80.0, 100, true);
         vehicles[1] = new HeavyTruck("Volvo", "FH16", 2019, Vehicle.nextVehicleID, true, 150.0, 300, false);
         vehicles[2] = new ElectricCar("Tesla", "Model 3", 2021, Vehicle.nextVehicleID, true, 70.0, 5, 4, 350, 80);
+        vehicles[3] = new ElectricCar("Rivian", "R1T", 2022, Vehicle.nextVehicleID, true, 90.0, 5, 4, 314, 85);
         vehicles[4] = new Motorcycle("Harley-Davidson", "Street 750", 2018, Vehicle.nextVehicleID, true, 40.0, 2, true, 750);
         vehicles[5] = new Motorcycle("Yamaha", "YZF-R3", 2021, Vehicle.nextVehicleID, true, 35.0, 2, false, 321);
         vehicles[6] = new LightTruck("Chevrolet", "Silverado", 2021, Vehicle.nextVehicleID, true, 85.0, 120, false);
